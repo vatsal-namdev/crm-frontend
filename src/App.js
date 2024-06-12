ec77da1cf548fdd26fc9f1f1369c995cea026c98
vatsal-namdev <114605872+vatsal-namdev@users.noreply.github.com>
2024-06-12 23:37:01 +0530

    nnav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import GoogleSignIn from './GoogleSignIn';
 import HomePage from './pages/HomePage';
 import CampaignPage from './pages/CampaignPage';
 import Navbar from './pages/Navbar';
 import './pages/Navbar.css';
 
-
 const App = () => {
+  const location = useLocation();
+
+  // Determine if the current path matches /aud or /campaigns
+  const showNavbar = location.pathname === '/aud' || location.pathname === '/campaigns';
+
+  return (
+    <div className="App">
+      {showNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<GoogleSignIn />} />
+        <Route path="/aud" element={<HomePage />} />
+        <Route path="/campaigns" element={<CampaignPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+const AppWrapper = () => {
   return (
     <Router>
-      <div className="App">
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<GoogleSignIn />} />
-          <Route path="/aud" element={<HomePage />} />
-          <Route path="/campaigns" element={<CampaignPage />} />
-        </Routes>
-      </div>
+      <App />
     </Router>
   );
 };
 
-export default App;
+export default AppWrapper;
